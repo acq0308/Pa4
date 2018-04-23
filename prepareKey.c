d1f2f7c3f74fa651ee8104581a0fa8ce3f67aad3
Andre' Robinson <andre3@g.clemson.edu>
2018-04-23 17:42:57 -0400

    Started work on prepareKey in C

---
+++ prepareKey.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		fprintf(stderr, "Proper usage: %s <key>\n", argv[0]);
+		exit(-1);
+	}
+	char alphabet[27];
+	char key[27];
+	int i,j=0;
+
+	for (i = 0; i < 26; i++)
+	{
+		key[i] = i + 'a';
+	}
+	alphabet[26] = '\0';
+
+	for (i = 0; argv[1][i] != '\0'; i++)
+	{
+		if ((argv[1][i] <= 'z' && argv[1][i] >= 'a') && alphabet[argv[1][i] - 'a'] != ' ') //Is the letter a lowercase number, and is it unused?
+		{
+			key[j] = argv[1][i];
+			alphabet[argv[1][i] - 'a'] = ' ';
+			j++;
+		}
+	}
+	for (i = 0; i < 26; i++)
+	{
+		if (alphabet[i] != ' ')
+		{
+			key[j] = alphabet[i];
+			j++;
+		}
+	}
+
+	printf("key: %s\n", key);
+	return 0;
+}
