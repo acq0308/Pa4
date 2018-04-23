eda93950fb6fa665815f3a0d7a6e9c537905f961
Andre' Robinson <andre3@g.clemson.edu>
2018-04-23 19:15:55 -0400

    - Realized the orphans were a bit too much - Re-implemented animal cruelty

--- prog4.s
+++ prog4.s
@@ -6,11 +6,12 @@
 @ Due: Wednesday, April 25th
 
 key	.req r0
-str     .req r1
+strn    .req r1
 encryp  .req r2
 decryp  .req r3
 count   .req r4
-
+alphab	.req r5
+keystr	.req r6
 
 	.file   "prog4.s"
         .text
@@ -22,16 +23,26 @@ count   .req r4
         .type   decode, %function
 
 .prepareKey:
-	push {r4, r5, lr}        	
-        
-        ldr r5, [r0]		// store dereference key into r5
-	mov r4, #27		// store 27 in r4 for character comparison
-        cmp r5, r4
-        bge next
-        
+	push {r4, r5, r6, lr}        	
+        sub sp, sp, #54			// make space for two arrays of 27 chars
+	ldr alphab, sp			// one array in r5 (alphabet)
+	add keystr, sp, #27		// second array in r6 (keystring)
+	mov count, #0
+        ldr r1, [r0]			// store dereference key into r1
+	
+	forLoop1:               	// Build the alphabet array
+		cmp count, #26
+		bge done
+		
+		add r2, count, #97	// count + 'a'
+		str r2, [alphab, count]
+			
+		add count, count, #1
+		b forLoop1
+	done:
 
-
-        pop  {r4,r5, pc}
+	add sp, sp, #54
+        pop  {r4, r5, r6, pc}
 .encode:
 	
 
