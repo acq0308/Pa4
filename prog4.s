63cab3a69b73a4483d5d01414dd668a24f927c6d
Andre' Chris Robinson <andre3@clemson.edu>
2018-04-24 17:33:08 -0400

    Creates alphabet on the stack

--- prog4.s
+++ prog4.s
@@ -5,13 +5,9 @@
 @ Rose Lowe
 @ Due: Wednesday, April 25th
 
-key     .req r0
 strn    .req r1
 encryp  .req r2
 decryp  .req r3
-count   .req r4
-alphab  .req r5
-keystr  .req r6
 
 	.file   "prog4.s"
         .text
@@ -20,6 +16,12 @@ keystr  .req r6
 	.type   prepareKey, %function
         .type   encode, %function
         .type   decode, %function
+
+key     .req r0
+count   .req r4
+alphab  .req r5
+keystr  .req r6
+
 prepareKey:
 	.fnstart
 	push {r4, r5, r6, lr}        	
@@ -42,11 +44,35 @@ prepareKey:
 	mov r2, #0
 	strb r2, [alphab, #26]         // Null terminate alphabet string
 
-  ldr r0, =fmtstr                // Print the alphabet before clearing the stack
-  mov r1, alphab
-  bl printf
+                                 // Create the key in keystr
+  mov count, #0                  // Count through key until null character
+  mov r2, #0
+  forLoop2:
+    ldrb r3, [key, count]        // Store current letter in r3
+                                 // Is r3 a lowercase letter?
+    cmp r3, #'a'                    // Is r3 above 'a'?
+    blt falseLower                 // If not, it's not a letter
+    cmp r3, #'z'                    // Is r3 below 'z'?
+    bgt falseLower                 // If not, it's not a letter
 
-	mov r0, alphab
+    trueLower:                   // r3 is a lowercase letter
+                                 // Is r3 already used in the key?
+      sub r4, r3, #'a'
+      ldrb r4, [alphab, r4]      // Find the right entry in alphab
+      cmp r4, #0                 // If entry is null, letter already used
+        trueUsed:                // What to do if entry is unused
+          ldr r5
+          b doneUsed
+
+        falseUsed:               // What to do if entry is used
+
+        doneUsed:
+      b doneIfLower
+    
+    falseLower:
+
+    doneIfLower:
+
 	add sp, sp, #54
   pop  {r4, r5, r6, pc}
 	.fnend
@@ -61,5 +87,3 @@ prepareKey:
   
 
 
-fmtstr:
-  .ascii "Alphabet in prog1.s: %s\n"
