afce41fa9dd31fdac3d46e69ab175dd466511f62
Austin Charles Queen <austinq@ada10.cs.clemson.edu>
2018-04-26 11:09:51 -0400

    changes to encode

--- encode.c
+++ encode.c
@@ -3,13 +3,29 @@
  
 char* encode(char * str, char const* key, char *encryp) 
 {
-   int count = 0;
+  /*int i;
+
+  for (i = 0; str[i] != '\0'; i++) //Scan through str until we get a null character
+  {
+    if (isAlphaLower(str[i]))
+    {
+      decode[i] = decodeSingleChar(str[i], key);
+    }
+    else decode[i] = str[i];
+  }
+  str[i] = '\0';
+  
+  int count = 0;*/
  
-   while (str[count] != '\0') {
-      str[count] -= 'a';
-      count++;
-   }
+   int count = 0;
+   for (count = 0; str[count] != '\0'; count++) 
+   {
+     if(str[count] >= 'a' && str[count] <= 'z')
+     {
+       str[count] -= 'a';
+     }
    return (str);
+   } 
 }
  
 int main() 
