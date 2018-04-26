08880f8d6ecacf181bae058fa7c714a2aa130bd0
Andre' Chris Robinson <andre3@clemson.edu>
2018-04-26 00:25:40 -0400

    Created decode.c, which includes a test driver as well   We will convert this into ARM assembly

---
+++ decode.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int isAlphaLower(char);
+char decodeSingleChar(char, char*);
+void decode(char *str, char *key, char *decode);
+
+
+int main()
+{
+  char encoded[] = "tpptad tp itvh";
+  char key[] = "trailbzescdfghjkmnopquvwxy";
+  char decoded[40] = "";
+
+  decode(encoded, key, decoded);
+
+  printf("encoded: %s\n", encoded);
+  printf("decoded: %s\n", decoded);
+}
+
+
+void decode(char *str, char *key, char *decode)
+{
+  int i;
+
+  for (i = 0; str[i] != '\0'; i++) //Scan through str until we get a null character
+  {
+    if (isAlphaLower(str[i]))
+    {
+      decode[i] = decodeSingleChar(str[i], key);
+    }
+    else decode[i] = str[i];
+  }
+  str[i] = '\0';
+
+}
+
+int isAlphaLower(char c)
+{
+  return (c <= 'z' && c >= 'a');
+}
+
+char decodeSingleChar(char c, char *key)
+{
+  char i = 0;
+
+  while (key[i] != c)
+  {
+    i++;
+  }
+
+  i += 'a';
+  return i;
+}
